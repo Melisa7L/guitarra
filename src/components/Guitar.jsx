890ff6181ea0f0e4546ba36e33168a7100fad632
Fátima Melisa Lino <117290413+Melisa7L@users.noreply.github.com>
2025-10-17 17:01:23 -0300

    cambios

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -2,11 +2,15 @@ export default function Guitar({guitar, addToCart}) {
 
     const { id, name, image, description, price } = guitar
 
-
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
-                <img className="img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
+
+                <img 
+                    className="img-fluid" 
+                    src={`/img/${image}.jpg`} 
+                    alt={`Guitarra modelo ${name}`} 
+                />
             </div>
             <div className="col-8">
                 <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
